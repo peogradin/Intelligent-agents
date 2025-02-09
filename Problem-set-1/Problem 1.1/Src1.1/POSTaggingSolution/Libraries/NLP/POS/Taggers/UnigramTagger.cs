c0ecf26570d27d76e7d0c1a5b87811bdfc22e1be
peogradin <peo.gradin@live.se>
2025-02-09 11:41:34 +0100

    Create UnigramTagger.cs

---
+++ Problem-set-1/Problem 1.1/Src1.1/POSTaggingSolution/Libraries/NLP/POS/Taggers/UnigramTagger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLP.POS.Taggers
+{
+    internal class UnigramTagger
+    {
+    }
+}
