0f053fcfdd13e4c8352b13a00e8b873a3d599c3c
peogradin <peo.gradin@live.se>
2025-02-15 21:13:22 +0100

    Tokenizer seems to work ok

---
+++ Problem-set-1/Problem 1.2/Src1.2/PerceptronClassifierSolution/Libraries/NLP/Tokenization/TokenizerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLP.Tokenization
+{
+    public class TokenizerTests
+    {
+        public static void RunTests()
+        {
+            Tokenizer tokenizer = new Tokenizer();
+            List<string> testSentences = new List<string>
+            {
+                "We were there all 12. We did ...",
+                "The price is $15.99 and €3.50!",
+                "Hello, Mr. Smith! How are you today?",
+                "NASA launched Apollo 11 on 16.07.1969.",
+                "I have 3.14 apples and 42.0 oranges.",
+                "Dr. Johnson is here. He arrived at 10:30 a.m."
+            };
+
+            foreach (string sentence in testSentences)
+            {
+                List<Token> tokens = tokenizer.Tokenize(sentence);
+                Console.WriteLine("Input: " + sentence);
+                Console.Write("Tokens: ");
+                foreach (var token in tokens)
+                {
+                    Console.Write("[" + token.Spelling + "] ");
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
--- Problem-set-1/Problem 1.2/Src1.2/PerceptronClassifierSolution/PerceptronClassifierApplication/Program.cs
+++ Problem-set-1/Problem 1.2/Src1.2/PerceptronClassifierSolution/PerceptronClassifierApplication/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//using NLP.Tokenization;
+//using System.Text;
 
 namespace PerceptronClassifierApplication
 {
@@ -17,6 +19,13 @@ namespace PerceptronClassifierApplication
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
+            //Console.WriteLine("Running Tokenizer Tests...\n");
+            //TokenizerTests.RunTests();
+
+            //Console.WriteLine("\nAll tests completed.");
+            //Console.ReadLine(); // Keep console open
+
         }
     }
 }
